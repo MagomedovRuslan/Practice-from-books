1524aa7b4a663985856ce1b6d6f1e5b1b04b4c95
MagomedovRuslan <magomedovruslan.dev@gmail.com>
2024-11-22 05:10:09 +0300

    Create binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,24 @@
+# binary_search
+user_id = [id for id in range(1, 1001)]
+
+
+def binary_search(check_list: list, item: int | float) -> int:
+    low = 0
+    high = len(check_list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = check_list[mid]
+
+        if guess == item:
+            return mid
+        
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    
+    return None
+
+
+print(binary_search(user_id, 499))
